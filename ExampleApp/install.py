b90c0ed445c6ba3c2f27865ed29e7a0f9313a0a6
Coder2354 <80308744+Coder2354@users.noreply.github.com>
2022-05-10 07:29:28 +0800

    Create install.py

---
+++ ExampleApp/install.py
@@ -0,0 +1,8 @@
+import os # lets you use command line features
+
+os.system('sudo apt-get examplePlsRemove -f') # example command
+# put sudo installs here like "os.system('sudo apt-get foo -f') and remember to add the -f flag please!
+# note that you can put as many installs as you like here but please don't make it take too long!
+
+# you can copy your files here, remember you can use the $PWD variable to use local paths.
+os.system('sudo cp $PWD/assets/foo.py /tmp') # example command
